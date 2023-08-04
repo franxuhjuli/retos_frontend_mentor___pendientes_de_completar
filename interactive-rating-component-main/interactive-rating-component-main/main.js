a00b5a9162411dab418fc1dfe27ca67946276bf6
franxuhjuli <52677153+franxuhjuli@users.noreply.github.com>
2023-08-04 18:30:15 +0200

    Add files via upload

---
+++ interactive-rating-component-main/interactive-rating-component-main/main.js
@@ -0,0 +1,14 @@
+function rate() {
+const mainContent = document.querySelector('#rating-state-content');
+const thankYouContent = document.querySelector('#thank-you-state-content');
+
+thankYouContent.classList.remove("hidden");
+mainContent.classList.add("hidden");
+}
+
+ let ratingNumber = document.getElementById('rating-items').ariaValueMax;
+document
+
+document.getElementById(
+    "rateNumber"
+  ).innerHTML = ratingNumber;
