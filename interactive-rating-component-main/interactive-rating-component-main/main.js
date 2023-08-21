e797236082c0a58c76747889f0bbb9a918c87c9e
franxuhjuli <52677153+franxuhjuli@users.noreply.github.com>
2023-08-21 15:16:08 +0200

    Add files via upload

--- interactive-rating-component-main/interactive-rating-component-main/main.js
+++ interactive-rating-component-main/interactive-rating-component-main/main.js
@@ -1,14 +1,36 @@
-function rate() {
-const mainContent = document.querySelector('#rating-state-content');
-const thankYouContent = document.querySelector('#thank-you-state-content');
+const submitButton = document.getElementById("submit-button");
 
-thankYouContent.classList.remove("hidden");
-mainContent.classList.add("hidden");
+function submitRate() {
+  const mainContent = document.querySelector('#rating-state-content');
+  const thankYouContent = document.querySelector('#thank-you-state-content');
+  thankYouContent.classList.remove("hidden");
+  mainContent.classList.add("hidden");
 }
 
- let ratingNumber = document.getElementById('rating-items').ariaValueMax;
-document
+submitButton.addEventListener("click", submitRate);
 
-document.getElementById(
-    "rateNumber"
-  ).innerHTML = ratingNumber;
+
+
+
+const oneStar = document.getElementById("1-star");
+const twoStars = document.getElementById("2-star");
+const threeStars = document.getElementById("3-star");
+const fourStars = document.getElementById("4-star");
+const fiveStars = document.getElementById("5-star");
+
+
+const rateNumber = document.getElementById("rate-number");
+
+
+function rate(num) {
+  let rated = num.textContent;
+  rateNumber.textContent = rated;
+  rateNumber.style.color = "hsl(0, 0%, 100%)";
+  rateNumber.style.backgroundColor = "hsl(217, 12%, 63%)";
+}
+
+oneStar.addEventListener("click", rate(oneStar));
+twoStars.addEventListener("click", rate(twoStars));
+threeStars.addEventListener("click", rate(threeStars));
+fourStars.addEventListener("click", rate(fourStars));
+fiveStars.addEventListener("click", rate(fiveStars));
